470adff140aee6a2a0e912d01b232bad64668b4a
iomitryako <iomitryako@mts.ru>
2023-08-06 14:10:20 +0400

    favicon fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,7 +10,7 @@ export const metadata = {
 const RootLayout = ({ children }) => (
   <html lang="en">
     <Head>
-      <link rel="icon" type="image/png" href="../resources/favicon.png" />
+      <link rel="icon" href="favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
