2a438256c4f33788f9b7e44a01dbad951d8c3207
iomitryako <iomitryako@mts.ru>
2024-05-14 13:55:47 +0400

    fams added

--- src/components/Main/data.js
+++ src/components/Main/data.js
@@ -3,11 +3,19 @@ import Telegram from '../../../resources/telegram.svg';
 import VK from '../../../resources/vk.svg';
 import Instagram from '../../../resources/instagram.svg';
 import Protonmail from '../../../resources/protonmail.svg';
+import Fams from '../../../resources/fams.png';
 
 export const companies = [
   {
     post: 'Frontend Developer',
-    companyName: 'Freelance',
+    companyName: 'fams.ai',
+    description: 'Media',
+    resources: [
+      {
+        src: Fams,
+        link: 'https://fams.ai/',
+      },
+    ],
   },
   {
     post: 'PR',
